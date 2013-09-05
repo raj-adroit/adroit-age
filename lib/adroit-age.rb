1a6d1e333c22c4cecee34e15080c6a101af1be58
raj-inevitable <raja.nice23@gmail.com>
2013-09-05 19:16:54 +0530

    Fix problem with Rails

--- lib/adroit-age.rb
+++ lib/adroit-age.rb
@@ -5,6 +5,5 @@ class AdroitAge
  	 	dob = dob.to_date
   		now = Time.now.utc.to_date
   		age = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
-  		puts "#{age}"
 	end
 end
