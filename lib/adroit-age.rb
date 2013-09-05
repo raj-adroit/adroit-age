86ace5ad83c1a939b1a7b5b5ed3e939599ffb888
raj-inevitable <raja.nice23@gmail.com>
2013-09-05 21:05:34 +0530

    Add Exception for Invalid date

--- lib/adroit-age.rb
+++ lib/adroit-age.rb
@@ -5,5 +5,7 @@ class AdroitAge
  	 	dob = dob.to_date
   		now = Time.now.utc.to_date
   		age = now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  		raise RuntimeError, 'adroit-age ~> invalid date - Please check the date' if age < 0  
+	    age
 	end
 end
