9964f54487bff530c957312c7772b359bd762c40
Raj Adroit <raja.nice23@gmail.com>
2013-12-29 23:17:09 +0530

    Find age with month, customize year/month labels

--- lib/adroit-age/api.rb
+++ lib/adroit-age/api.rb
@@ -2,11 +2,45 @@ module Adroit
 	
 	class Age
 		attr_accessor :dob, :now, :age
-
-		def find dob
-	  		@now = Time.now.utc.to_date
-	  		@age = @now.year - dob.year - ((@now.month > dob.month || (@now.month == dob.month && @now.day >= dob.day)) ? 0 : 1)
+		@@now = Time.now.utc.to_date
+		def find_year dob
+	  	age = @@now.year - dob.year - ((full_year? dob) ? 0 : 1)
 		end
+
+		def find_full dob, year_cust, month_cust
+			year = find_year dob
+      month = find_month dob
+      month_cust_label = month_cust.nil? ? "#{month_label month}" : month_cust
+      year_cust_label = year_cust.nil? ? "#{year_label year}" : year_cust
+			"#{year} #{year_cust_label} and #{month} #{month_cust_label}"
+    end
+
+    def find_month dob
+    	if @@now.month == dob.month 
+				0
+			elsif @@now.month < dob.month
+				((12 - dob.month) + @@now.month) - ((full_month? dob.day) ? 0 : 1)
+			elsif @@now.month > dob.month
+				(@@now.month - dob.month) - ((full_month? dob.day) ? 0 : 1)
+			end
+    end
+
+    def year_label year
+    	year == 1 ? "years" : "year"
+    end
+
+    def month_label month
+    	month == 1 ? "months" : "month" 
+    end
+
+    def full_month? day
+    	(@@now.day > day || @@now.day == day)
+    end
+
+    def full_year? dob
+    	(@@now.month > dob.month || (@@now.month == dob.month && @@now.day >= dob.day))
+    end
+
 	end
 
 end
--- lib/adroit-age/date.rb
+++ lib/adroit-age/date.rb
@@ -1,7 +1,11 @@
 class Date
 
 	def find_age
-		Adroit::Age.new.find self
+		Adroit::Age.new.find_year self
+	end
+
+	def find_full_age year_cust = nil, month_cust = nil
+		Adroit::Age.new.find_full self, year_cust, month_cust
 	end
 
 end
