dec9bc5169eafecc73e397f22f101e50c6c9bebc
Raj Adroit <raja.nice23@gmail.com>
2013-12-29 20:03:16 +0530

    [patch] remove check its string/date

--- lib/adroit-age/api.rb
+++ lib/adroit-age/api.rb
@@ -4,9 +4,8 @@ module Adroit
 		attr_accessor :dob, :now, :age
 
 		def find dob
-			@dob = (dob.is_a? String) ? Date.parse(dob) : dob
 	  		@now = Time.now.utc.to_date
-	  		@age = @now.year - @dob.year - ((@now.month > @dob.month || (@now.month == @dob.month && @now.day >= @dob.day)) ? 0 : 1)
+	  		@age = @now.year - dob.year - ((@now.month > dob.month || (@now.month == dob.month && @now.day >= dob.day)) ? 0 : 1)
 		end
 	end
 
