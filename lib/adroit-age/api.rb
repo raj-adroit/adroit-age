cb66a0160f4e49897f3441dd21ac7a952ecd870d
Rajendran <raja.nice23@gmail.com>
2013-12-30 19:39:28 +0530

    fix bugs, update readme

--- lib/adroit-age/api.rb
+++ lib/adroit-age/api.rb
@@ -1,18 +1,19 @@
 module Adroit
-	
-	class Age
-		attr_accessor :dob, :now, :age
-		@@now = Time.now.utc.to_date
-		def find_year dob
-	  	age = @@now.year - dob.year - ((full_year? dob) ? 0 : 1)
-		end
+  
+  class Age
 
-		def find_full dob, year_cust, month_cust
-			year = find_year dob
-      month = find_month dob
-      month_cust_label = month_cust.nil? ? "#{month_label month}" : month_cust
-      year_cust_label = year_cust.nil? ? "#{year_label year}" : year_cust
-			"#{year} #{year_cust_label} and #{month} #{month_cust_label}"
+    attr_accessor :dob, :now, :age
+	@@now = Time.now.utc.to_date
+	def find_year dob
+	   age = @@now.year - dob.year - ((full_year? dob) ? 0 : 1)
+	end
+
+	def find_with_month dob, year_cust, month_cust
+	    year = find_year dob
+        month = find_month dob
+        month_cust_label = month_cust.nil? ? "#{month_label month}" : month_cust
+        year_cust_label = year_cust.nil? ? "#{year_label year}" : year_cust
+		"#{year} #{year_cust_label} and #{month} #{month_cust_label}"
     end
 
     def find_month dob
@@ -26,11 +27,11 @@ module Adroit
     end
 
     def year_label year
-    	year == 1 ? "years" : "year"
+    	year == 1 ? "year" : "years"
     end
 
     def month_label month
-    	month == 1 ? "months" : "month" 
+    	month == 1 ? "month" : "months" 
     end
 
     def full_month? day
